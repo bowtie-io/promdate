6eaa5f234d7ffab6c224653ac00e0fc5511c38dd
Chad Person <chad@seedworthy.com>
2015-04-09 16:35:07 -0600

    updated on/off states and navs for logged in status]

--- js/authentication.js
+++ js/authentication.js
@@ -1,12 +1,10 @@
-$(function(){
-  /* Controls based on user's session
-   * NOTE: Do not use this to hide sensitive elements - this is client side only
-   *       and designed for display of navigation options */
-  bowtie.user.info(function(user){
-    if(user){
-      $('.signed-in').show();
-    }else{
-      $('.signed-out').show();
-    }
-  });
+/* Controls based on user's session
+ * NOTE: Do not use this to hide sensitive elements - this is client side only
+ *       and designed for display of navigation options */
+bowtie.user.info(function(user){
+  if(user){
+    $('.signed-in').show();
+  }else{
+    $('.signed-out').show();
+  }
 });
