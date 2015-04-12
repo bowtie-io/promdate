8cc06f967df3fc6550daf806b2ba0779c6d8d871
Isaiah <isaiah@atpay.com>
2015-04-11 23:48:40 -0600

    Starts linking up matching page

--- js/datamatch.js
+++ js/datamatch.js
@@ -73,6 +73,76 @@ function dmProject(project_sid) {
 
 
 
+
+
+
+
+
+
+    this.get_actual_matches = function () {
+
+      $.ajax({
+          type: "GET",
+          url: "/matches",
+          cache: false,
+          data: {project_sid: project_sid},
+          success: function (data) {
+            console.log(data);
+            show_matches(data.matches);
+          },
+          error: function (xhr, status, err) {
+            console.log(status);
+          },
+       dataType: "json"
+      });
+
+      function show_matches (potentials) {
+        $.each(potentials, function(index, value) {
+
+          $.ajax({
+              type: "GET",
+              url: "/dm_users/"+value,
+              cache: false,
+              data: {project_sid: project_sid},
+              success: function (data) {
+                console.log(data);
+                fill_match(data)
+              },
+              error: function (xhr, status, err) {
+                console.log("something is wrong")
+              },
+           dataType: "json"
+          });
+          function fill_match(details){
+            potential_data = "<li class='"+value+"'><img src='http://img2.wikia.nocookie.net/__cb20140427211725/dragcave/images/6/6e/No_avatar.jpg' class='avatar'><br /> id:"+value+" <br /> <strong>Name:</strong><br />"+details.user.name+" <br /> <strong>Contact:</strong><br />"+details.user.email+" <br /><strong>Details: </strong><br> "+details.details+"<br /> </li>";
+            $("#actual_matches").empty();
+
+            $("#actual_matches").append(potential_data);
+          }
+        });
+      }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     $("html").on("click", "a#info", function(e) {
       e.preventDefault();
       $(this).parent().parent().children(".info_block").slideToggle();
@@ -118,55 +188,3 @@ function dmProject(project_sid) {
     });
 
 }
-
-
-
-
-
-
-
-
-//
-// function get_matches () {
-// $.ajax({
-//     type: "GET",
-//     url: "/matches",
-//     cache: false,
-//     data: {project_sid: project_sid},
-//     success: function (data) {
-//       console.log(data);
-//       show_matches(data.matches);
-//     },
-//     error: function (xhr, status, err) {
-//       console.log(status);
-//     },
-//  dataType: "json"
-// });
-//
-// function show_matches (potentials) {
-//   $.each(potentials, function(index, value) {
-//
-//     $.ajax({
-//         type: "GET",
-//         url: "/dm_users/"+value,
-//         cache: false,
-//         data: {project_sid: project_sid},
-//         success: function (data) {
-//           console.log(data);
-//           fill_match(data)
-//         },
-//         error: function (xhr, status, err) {
-//           console.log("something is wrong")
-//         },
-//      dataType: "json"
-//     });
-//     function fill_match(details){
-//       potential_data = "<li class='"+value+"'><img src='http://img2.wikia.nocookie.net/__cb20140427211725/dragcave/images/6/6e/No_avatar.jpg' class='avatar'><br /> id:"+value+" <br /> <strong>Name:</strong><br />"+details.user.name+" <br /> <strong>Contact:</strong><br />"+details.user.email+" <br /><strong>Details: </strong><br> "+details.details+"<br /> </li>";
-//       $("#actual_matches").empty();
-//
-//       $("#actual_matches").append(potential_data);
-//     }
-//   });
-// }
-//
-// }
