a8f7f7dace96a1841c58714a7dee13f8de2194b6
James Kassemi <jkassemi@gmail.com>
2015-05-04 11:21:58 -0600

    countdown.js from latest template

--- js/countdown.js
+++ js/countdown.js
@@ -1,13 +1,19 @@
-$(function(){
+$(document).on("bowtie:splash-rendered", function(){
   $('[data-countdown]').each(function() {
      var $this = $(this), finalDate = $(this).data('countdown');
+     var original = $(this).html();
 
      $this.countdown(finalDate, function(event) {
-       $this.html(event.strftime(''
-         + '<span class="count"><span class="count-wrap"><span class="digits" id="days">%D</span> day%!d </span></span>'
-         + '<span class="count"><span class="count-wrap"><span class="digits" id="hours">%H</span> hr </span></span>'
-         + '<span class="count"><span class="count-wrap"><span class="digits" id="minutes">%M</span> min </span></span>'
-         + '<span class="count"><span class="count-wrap"><span class="digits" id="seconds">%S</span> sec</span></span>'));
+       if(event.type == "finish"){
+         $this.html(original);
+       }else{
+         $this.html(event.strftime(''
+           + '<span class="count"><span class="count-wrap"><span class="digits" id="days">%D</span> day%!d </span></span>'
+           + '<span class="count"><span class="count-wrap"><span class="digits" id="hours">%H</span> hr </span></span>'
+           + '<span class="count"><span class="count-wrap"><span class="digits" id="minutes">%M</span> min </span></span>'
+           + '<span class="count"><span class="count-wrap"><span class="digits" id="seconds">%S</span> sec</span></span>'));
+       }
      });
  });
 });
+
